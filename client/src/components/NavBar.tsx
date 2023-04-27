2d2a7813f03048dd48c07e885a58b6d0d43693e0
Atanda remilekun <atandaremilekun@gmail.com>
2023-04-27 20:41:55 +0100

    added avatar component

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
 import SideMenu from "./SideMenu";
+import { Avatar } from "@mantine/core";
+import { userSlice } from "../zustand/user";
 
 function NavBar() {
   const [isMenu, setIsMenu] = useState(false);
+  const user = userSlice((state) => state.user);
   return (
     <nav className="flex items-center justify-between mx-auto max-w-[1400px] px-5 py-4 md:px-[48px] lg:px-[50px] xl:px-[65px] relative">
       <div>logohere</div>
       <button
         onClick={() => setIsMenu(!isMenu)}
-        className="flex items-center justify-center p-2 rounded-xl gap-2 shadow-xl"
+        className="flex items-center justify-center p-1 rounded-[20px] gap-1 shadow-xl"
       >
         <div>hello</div>
-        <div>hello</div>
+        <Avatar radius="xl" src={user?.image} />
       </button>
 
       {isMenu && <SideMenu setIsMenu={setIsMenu} />}
