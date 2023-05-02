b33be78ca5ab0342162097adb2b6874798d82d7f
Atanda remilekun <atandaremilekun@gmail.com>
2023-05-02 17:51:09 +0100

    favourite toggle

--- client/src/components/PropertyCard.tsx
+++ client/src/components/PropertyCard.tsx
@@ -1,17 +1,29 @@
 import { Link } from "react-router-dom";
 import { Property } from "../typings";
 import { favouritesSlice } from "../zustand/userFavourites";
+import { isFavourite } from "../util/checkFavourites";
 
 function PropertyCard({ _id, city, country, images, price }: Property) {
   const addFavourite = favouritesSlice((state) => state.addFavourite);
+  const removeFavourite = favouritesSlice((state) => state.removeFavourite);
   const singleImage = images[0];
 
   return (
     <div className="relative ">
-      <div
-        className="w-10 h-10 rounded-full top-5 right-5 bg-green-500 absolute cursor-pointer"
-        onClick={() => addFavourite({ _id, city, country, singleImage, price })}
-      />
+      {isFavourite(_id) ? (
+        <div
+          className="w-10 h-10 rounded-full top-5 right-5 bg-red-500 absolute cursor-pointer"
+          onClick={() => removeFavourite(_id)}
+        />
+      ) : (
+        <div
+          className="w-10 h-10 rounded-full top-5 right-5 bg-green-500 absolute cursor-pointer"
+          onClick={() =>
+            addFavourite({ _id, city, country, singleImage, price })
+          }
+        />
+      )}
+
       <Link to={`/apartment/${_id}`} className="flex flex-col gap-3">
         <div className="h-[300px]">
           <img
