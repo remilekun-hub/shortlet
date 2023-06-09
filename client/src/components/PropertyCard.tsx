e9548596551068e5afa7f2cb0fcac34d3a8273bc
Atanda remilekun <atandaremilekun@gmail.com>
2023-06-09 16:12:40 +0100

    added image lazyLoading

--- client/src/components/PropertyCard.tsx
+++ client/src/components/PropertyCard.tsx
@@ -7,6 +7,9 @@ import useLoginModalState from "../zustand/UseLoginModal";
 import Button from "./Button";
 import { useMemo } from "react";
 import { format } from "date-fns";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import { useNavigate } from "react-router-dom";
 
 function PropertyCard({
   label,
@@ -16,6 +19,7 @@ function PropertyCard({
 }: PropertyCardProps) {
   const user = userSlice((state) => state.user);
   const loginModal = useLoginModalState();
+  const navigate = useNavigate();
 
   const reservationDate = useMemo(() => {
     if (!reservation) return null;
@@ -28,24 +32,21 @@ function PropertyCard({
 
   return (
     <div className="relative ">
-      {user ? (
-        <ToggleHearts id={data._id} />
-      ) : (
-        <div
-          className="absolute top-4 right-[11px] cursor-pointer w-8 h-8 rounded-full"
-          onClick={loginModal.onOpen}
-        >
-          <HeartIcon />
-        </div>
-      )}
-
-      <Link to={`/apartment/${data._id}`} className="flex flex-col gap-3">
+      <div
+        className="flex flex-col gap-3"
+        onClick={() => navigate(`/apartment/${data._id}`)}
+      >
         <div className="h-[300px]">
-          <img
+          <LazyLoadImage
             src={data.images[0]}
-            className="object-cover h-full w-full rounded-xl"
+            className="object-cover h-full w-full rounded-xl absolute z-[-200]"
+            effect="blur"
+            height={300}
+            width={"100%"}
+            alt={`apartment ${data._id} image`}
           />
         </div>
+
         <div className="">
           <h3 className="text-[16px] text-black font-semibold capitalize">{`${data.city}, ${data.state}, ${data.country}`}</h3>
           <h3 className="text-neutral-500 text-[14px] my-[5px]">
@@ -58,7 +59,19 @@ function PropertyCard({
             <span className="text-neutral-600 ml-[6px]">night</span>
           </p>
         </div>
-      </Link>
+        <div onClick={(e) => e.stopPropagation()}>
+          {user ? (
+            <ToggleHearts id={data._id} />
+          ) : (
+            <div
+              className="absolute top-4 right-[11px] cursor-pointer w-8 h-8 rounded-full"
+              onClick={loginModal.onOpen}
+            >
+              <HeartIcon />
+            </div>
+          )}
+        </div>
+      </div>
       {label && onSubmit && (
         <div className="mt-2">
           <Button label={label} onSubmit={onSubmit} />
